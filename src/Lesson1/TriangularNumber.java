8d4f3e625dbc01c92d2e4ad2207ac7b62833d841
Vera Bylinskaia <iagyar@list.ru>
2024-10-18 19:58:36 +0300

    Задание 1

---
+++ src/Lesson1/TriangularNumber.java
@@ -0,0 +1,17 @@
+package Lesson1;
+
+public class TriangularNumber {
+    public int num;
+    public int rez;
+
+
+
+    public TriangularNumber(int num){
+        this.num = num;
+        int sum = 0;
+        for( int i = 1; i < num; i++){
+            sum += i;
+        }
+        this.rez = sum;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Lesson1.TriangularNumber;
+
+public class Main {
+
+//    1. Вычислить n-ое треугольного число(сумма чисел от 1 до n), n!
+//            (произведение чисел от 1 до n)
+
+}
